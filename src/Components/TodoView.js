7bac0ba7cf3222b111a1eeb3bacfea6bac2f92a4
imranhossainemi <blur-imran@hotmail.com>
2023-04-03 16:30:00 +0600

    Update TodoView.js

--- src/Components/TodoView.js
+++ src/Components/TodoView.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const TodoView = () => (
-  <div />
+  <main>
+    <form className="form" id="form">
+      <input id="task" name="task" type="text" placeholder="Write Task Here" />
+    </form>
+  </main>
 );
 
 export default TodoView;
