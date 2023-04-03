86fc52ce3adcf48f6180073e61a737d87f632cbe
imranhossainemi <blur-imran@hotmail.com>
2023-04-03 16:57:38 +0600

    Update TodoView.js

--- src/Components/TodoView.js
+++ src/Components/TodoView.js
@@ -4,6 +4,7 @@ const TodoView = () => (
   <main>
     <form className="form" id="form">
       <input id="task" name="task" type="text" placeholder="Write Task Here" />
+      <button type="submit" className="btnSubmit">ADD</button>
     </form>
   </main>
 );
