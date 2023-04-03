38a9e934e3a6117054ed5a98dd4121540315742a
imranhossainemi <blur-imran@hotmail.com>
2023-04-03 22:10:02 +0600

    Update ToDoItem.js

--- src/Components/ToDoItem.js
+++ src/Components/ToDoItem.js
@@ -1,16 +1,27 @@
-/* eslint-disable react/prop-types */
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const ToDoItem = ({ itemProp, handleChange }) => (
+const ToDoItem = ({ itemProp, handleChange, delTodo }) => (
   <li>
-    <input
-      type="checkbox"
-      checked={itemProp.completed}
-      onChange={() => handleChange(itemProp.id)}
-    />
-    <button type="button"> Delete </button>
+    <>
+      <input
+        type="checkbox"
+        checked={itemProp.completed}
+        onChange={() => handleChange(itemProp.id)}
+      />
+      <button onClick={() => delTodo(itemProp.id)} type="button">
+        Delete
+      </button>
+
+    </>
     {itemProp.title}
   </li>
 );
+
+ToDoItem.propTypes = {
+  itemProp: PropTypes.string.isRequired,
+  handleChange: PropTypes.string.isRequired,
+  delTodo: PropTypes.string.isRequired,
+};
 
 export default ToDoItem;
