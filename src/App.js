ada360e01084a0fc805080e51285ea2daa020b70
imranhossainemi <blur-imran@hotmail.com>
2023-04-03 16:29:53 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Header from './Components/Header';
+import TodoView from './Components/TodoView';
 
 function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <TodoView />
+    </>
   );
 }
 
