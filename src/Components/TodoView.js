f4a21240a7e428c03c8e8f7834dce828bef5a6e6
imranhossainemi <blur-imran@hotmail.com>
2023-04-03 22:10:15 +0600

    Update TodoView.js

--- src/Components/TodoView.js
+++ src/Components/TodoView.js
@@ -4,10 +4,6 @@ import TodoLogic from './TodoLogic';
 const TodoView = () => (
   <main>
     <TodoLogic />
-    {/* <form className="form" id="form">
-      <input id="task" name="task" type="text" placeholder="Write Task Here" />
-      <button type="submit" className="btnSubmit">ADD</button>
-    </form> */}
   </main>
 );
 
