96d0a4e42976c77a38951fed5194d269138fc7b6
imranhossainemi <blur-imran@hotmail.com>
2023-04-03 22:09:57 +0600

    Update InputToDo.js

--- src/Components/InputToDo.js
+++ src/Components/InputToDo.js
@@ -1,9 +1,40 @@
-import React from 'react';
+/* eslint-disable react/prop-types */
+import React, { useState } from 'react';
 
-const InputToDo = () => (
-  <div>
-    Input here
-  </div>
-);
+const InputToDo = ({ addTodoItem }) => {
+  const [title, setTitle] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleChange = (e) => {
+    setTitle(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (title.trim()) {
+      addTodoItem(title);
+      setTitle('');
+      setMessage('');
+    } else {
+      setMessage('Please add item');
+    }
+  };
+  return (
+    <>
+      <form className="form" id="form" onSubmit={handleSubmit}>
+        <input
+          id="task"
+          name="task"
+          type="text"
+          placeholder="Write Task Here"
+          value={title}
+          onChange={handleChange}
+        />
+        <button type="submit" className="btnSubmit">ADD</button>
+      </form>
+      <h2>{message}</h2>
+
+    </>
+  );
+};
 
 export default InputToDo;
