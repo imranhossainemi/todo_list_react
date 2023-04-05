8563d8b98a4d04acc866b61b58d9a0b10e532ea9
imranhossainemi <blur-imran@hotmail.com>
2023-04-05 19:23:37 +0600

    Update ToDoItem.js

--- src/Components/ToDoItem.js
+++ src/Components/ToDoItem.js
@@ -1,20 +1,66 @@
-import React from 'react';
+/* eslint-disable react/prop-types */
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-const ToDoItem = ({ itemProp, handleChange, delTodo }) => (
-  <li className="listItem">
-    <input
-      type="checkbox"
-      checked={itemProp.completed}
-      onChange={() => handleChange(itemProp.id)}
-      className="taskCmp"
-    />
-    {itemProp.title}
-    <button onClick={() => delTodo(itemProp.id)} type="button" className="taskDel">
-      Delete
-    </button>
-  </li>
-);
+const ToDoItem = ({
+  itemProp, handleChange, delTodo, setUpdate,
+}) => {
+  const [updateInput, setUpdateInput] = useState(itemProp.title);
+  const completedStyle = {
+    fontStyle: 'italic',
+    color: '#595959',
+    opacity: 0.4,
+    textDecoration: 'line-through',
+  };
+  const [editing, setEditing] = useState(false);
+  const handleEditing = () => {
+    setEditing(true);
+  };
+  const viewMode = {};
+  const editMode = {};
+  if (editing) {
+    viewMode.display = 'none';
+  } else {
+    editMode.display = 'none';
+  }
+  const handleUpdatedDone = (event) => {
+    if (event.key === 'Enter') {
+      setUpdate(updateInput, itemProp.id);
+      setEditing(false);
+    }
+  };
+  return (
+    <li className="listItem">
+      <div className="listContent" style={viewMode}>
+        <input
+          type="checkbox"
+          checked={itemProp.completed}
+          onChange={() => handleChange(itemProp.id)}
+          className="taskCmp"
+        />
+        <span style={itemProp.completed ? completedStyle : null}>
+          {updateInput}
+        </span>
+        <div className="btns">
+          <button onClick={handleEditing} type="button" className="taskEdt">
+            Edit
+          </button>
+          <button onClick={() => delTodo(itemProp.id)} type="button" className="taskDel">
+            Delete
+          </button>
+        </div>
+      </div>
+      <input
+        type="text"
+        value={updateInput}
+        className="textEdt"
+        style={editMode}
+        onChange={(e) => setUpdateInput(e.target.value)}
+        onKeyDown={handleUpdatedDone}
+      />
+    </li>
+  );
+};
 
 ToDoItem.propTypes = {
   itemProp: PropTypes.string.isRequired,
