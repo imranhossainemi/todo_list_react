8d048a33d7360695327a60de08e74e3788e8e740
imranhossainemi <blur-imran@hotmail.com>
2023-04-05 11:35:41 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import Header from './Components/Header';
 import TodoView from './Components/TodoView';
 
 function App() {
   return (
     <>
-      <Header />
       <TodoView />
     </>
   );
